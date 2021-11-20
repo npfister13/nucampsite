438e39116eff44c371f29a0c3c4654bcb5203644
Nicholas Pfister <npfister13@hotmail.com>
2021-11-20 09:42:36 -0500

    workshop 4

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,12 @@ $(function(){
         
     });
 });
+$(function(){
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('show');
+    })
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('show');
+    });
+})
 
